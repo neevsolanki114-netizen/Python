1c9831354a27d79c4d9e865555d70f302cb71c7d
neevsolanki114-netizen <neevsolanki114@gmail.com>
2026-09-16 16:24:33 +0530

    code added

---
+++ 03_operators/04_logical.py
@@ -0,0 +1,138 @@
+# and
+
+# In AND operator has both condition to be True .
+
+age=18
+
+drivinglicense="You can drive the vehicle"
+
+if (age>=18 and drivinglicense):
+    print("You can drive the vehicle")
+
+else :
+    print("you can't drive the vehicle")   
+
+
+
+# 2nd Condition
+
+if (age>18 and drivinglicense):
+    print("you can drive the vehicle")  
+else :
+    print("you can't drive the vehicle")     
+
+
+
+age=60
+
+senior="You are senior citizen"
+
+if (age>=60 and senior):
+    print("you are senior citizen")
+
+else :
+    print("you are not senior citizen")
+
+
+# 2nd condition
+
+if (age>60 and senior):
+    print("you are senior citizen")
+
+else:
+    print("you are not senior citizen")
+
+
+
+age=18
+
+minor="you are minor"
+
+
+if (age<=18 and minor):
+    print("you are eligable to vote")
+
+else: 
+    print("you are not eligable to vote")
+
+
+    # 2nd condition
+
+if (age<18 and minor):
+    print("you are eligable to vote")
+else:
+    print("you are not eligable to vote")
+\
+# Or
+
+# In or Operators there are 2 condition 
+# In this operator one condition is true & another one is false
+
+Student_No=1234
+Teacher="Shivam Sir"
+
+
+if (Student_No==1234 or Teacher=="Shivam Sir"):
+    print("Today is Shivam Sir's Lecture")
+
+else:
+   print ( "Today is Prashant Sir's Lecture" )
+
+
+# 2nd condition 
+
+if (Student_No==5677 or Teacher=="Prashant Sir"):
+    print("Today is Shivam Sir's Lecture")
+
+else:
+   print ( "Today is Prashant Sir's Lecture" )
+
+
+age>18
+license="Eligible to drive "
+license1="Not Eligible"
+
+if (age>18 or license):
+    print("You are eligible to drive ")
+
+else:
+    print("you are not eligible to drive")
+
+
+# 2nd con 
+
+if (age<17 or license1=="Eligible to drive "):
+    print("You are eligible to drive ")
+else:
+    print("you are not eligible to drive")
+
+age=60
+citizen="Senior"
+citizen1="Minor"
+
+if (age>=60 or citizen):
+    print("you are senior citizen")
+else:
+    print("you are not senior citizen")
+
+# 2nd condition
+
+if (age<60 or citizen1=="Senior"):
+    print("you are senior citizen")
+else:
+    print("you are not senior citizen")
+
+# 3 Not Condition 
+
+Jaggu=False
+
+print(not Jaggu)
+
+Jaggu=True
+
+print(not Jaggu)
+
+
+    
+
+
---
+++ 03_operators/05_membership.py
@@ -0,0 +1,41 @@
+Fruits=['Mango','Orange','Cherry']
+print("Mango" in Fruits )
+print("Grapes")
+
+Vegetables=['carrot','potatos','tomato']
+print("carrot" in Vegetables )
+
+print("onion" in Vegetables )
+
+car=['BMW','AUDI','RR']
+print("RR" in car )
+
+print("Tata" in car)
+
+# 2. Not in operator
+
+
+print("Not In")
+
+
+
+Fruits=['dragonfruits','guavava','kiwi']
+
+print("Mango" not in Fruits)
+
+print("kiwi" not in Fruits)
+
+
+vegetable=['onion','ginger','cornflower']
+
+print("potato" not in vegetable)
+
+print("onion" not in vegetable)
+
+
+car=['Toyota','Hyundai','Porsche']
+
+print("RR" not in car)
+
+print("Hyundai" not in car)
+
