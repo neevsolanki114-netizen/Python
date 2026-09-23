446a1556f5470ed5ea2261ff18b2c9bb2fef55e4
neevsolanki114-netizen <neevsolanki114@gmail.com>
2026-09-23 16:43:03 +0530

    type casting & control structure code added

---
+++ 04_type_casting/02_comment.py
@@ -0,0 +1,20 @@
+# Comment is used for explanation 
+
+# in python there is only single line comments can be done using #
+
+
+# Hi
+# Everyone
+# How are you 
+
+
+# this is not multiline 
+
+...
+# Noob
+# Pro
+# Ultra Pro
+...
+
+
+# This Comment has been not in use
---
+++ 05_control_structure/02_if.py
@@ -0,0 +1,17 @@
+Mark=32 
+
+if (Mark<=32):
+    print("You have been passed")
+
+
+
+Age=15
+
+if (Age<15):
+    print("You are not eligable to vote")
+
+
+age=80
+
+if (Age>=80):
+    print("You are Senior Citizen")
